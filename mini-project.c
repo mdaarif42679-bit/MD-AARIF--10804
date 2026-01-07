5eb34a22a92836ffcacf140d34d2fdaa2331026c
mdaarif42679-bit <mdaarif42679@gmail.com>
2026-01-07 17:59:03 +0530

    Add time conversion from seconds to HH:MM:SS format

---
+++ mini-project.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    long int totalSeconds;
+    int hours, minutes, seconds;
+
+    printf("Enter total seconds: ");
+    scanf("%ld", &totalSeconds);
+
+    hours = totalSeconds / 3600;
+    minutes = (totalSeconds % 3600) / 60;
+    seconds = totalSeconds % 60;
+
+    printf("Formatted Time (HH:MM:SS) = %02d:%02d:%02d\n", hours, minutes, seconds);
+
+    return 0;
+}
